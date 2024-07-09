5a2c3e9a231d66320189fcab5e93cf5f86b568ed
youssef-elhoubi <elhoubiyoussef@gmail.com>
2024-07-09 12:17:26 +0100

    Added UserSchema model and exported connectDB function

---
+++ server/models/UserSchema.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const {Schema,model} = mongoose
+
+const UserSchema = new Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    profileComplaited:{type:Boolean,default:false},
+    newsLatterSubscribed:{type:Boolean,default:false},
+    createdAt:{type:Date,default:Date.now},
+    updatedAt:{type:Date,default:Date.now},
+},{timestamps:true});
+
+export default model("User",UserSchema)
