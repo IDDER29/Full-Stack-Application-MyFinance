5e104c78ab2b28e06cf3ff9b9eb03c573b4719c3
youssef-elhoubi <elhoubiyoussef@gmail.com>
2024-07-11 02:30:06 +0100

    Updated userController.js to create new user with hashed password and JWT token, and refactored models (UserSchema, itemSchima, profileschima) to use consistent syntax and exports.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,27 @@
 import conectDB from "../models/connection";
-import {User} from "../models/UserSchema"
+import { User } from "../models/UserSchema";
+import createtoken from "../utils/jwt";
+import { StatusCodes } from "http-status-codes";
+import hashPassword from "../utils/hashPassWord"
 
 import conectDB from "../models/connection";
 
 conectDB()
 
+const newUser = async (req, res) => {
+    const { name, email, password } = req.body
+    if (!name || !email || !password) {
+        return res.status(StatusCodes.BAD_REQUEST).json({error:"you have messed some fealeds please check all the fealdes"})
+    }
+    const thepasseorde = hashPassword(password)
+    const user =  User.create({
+        name,
+        email,
+        password:thepasseorde
+    })
+    const token = createtoken(user)
+    return res.status(StatusCodes.CREATED).json({token})
+}
 
 
 const newUSer = async()
