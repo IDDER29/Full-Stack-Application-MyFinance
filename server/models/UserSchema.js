48d13f9107df6141e11b7375111400eb8a064e9b
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-07-15 13:09:42 +0100

    Update UserSchema.js

--- server/models/UserSchema.js
+++ server/models/UserSchema.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
-const {Schema,model} = mongoose
+const { Schema, model } = mongoose
 
 const UserSchema = new Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
-    profileComplaited:{type:Boolean,default:false},
-    newsLatterSubscribed:{type:Boolean,default:false},
-    createdAt:{type:Date,default:Date.now},
-    updatedAt:{type:Date,default:Date.now},
-},{timestamps:true});
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profileComplaited: { type: Boolean, default: false },
+    newsLatterSubscribed: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+}, { timestamps: true });
 
-export const User = model("User",UserSchema)
+export const User = model("User", UserSchema)
