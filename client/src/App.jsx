6379226b70f584d51802110068f630e9d3c99f4b
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-07-16 22:01:44 +0100

    change the place of login and sign up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,20 @@
-import FormLogin from "./components/signup-in/LogeIn";
-import FormRegistration from "./components/signup-in/SignUp"
+import FormLogin from "./pages/LogeIn";
+import FormRegistration from "./pages/SignUp";
 import NotFound from "./components/notfound/notfound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<FormRegistration/>}></Route>
-        <Route path="/login" element={<FormLogin/>}></Route>
-        <Route path='*' element={<NotFound />}/>
-      </Routes>
+        <Routes>
+          <Route path="/signup" element={<FormRegistration />}></Route>
+          <Route path="/login" element={<FormLogin />}></Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
