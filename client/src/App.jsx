a518db34734b20a1e003aa30db03ae3a99414191
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2024-07-21 17:45:07 +0100

    update the way of the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,22 +12,23 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<PrivateRoute Component={Home} />}></Route>
+        {/* Public Routes */}
+        <Route path="/signup" element={<FormRegistration />} />
+        <Route path="/login" element={<FormLogin />} />
+        <Route path="/" element={<Landing />} />
+
+        {/* Private Routes */}
+        <Route path="/home" element={<PrivateRoute Component={Home} />} />
         <Route
           path="/dashboard"
           element={<PrivateRoute Component={Dashboard} />}
-        ></Route>
-        <Route path="/signup" element={FormRegistration}></Route>
-        <Route path="/login" element={FormLogin}></Route>
-
-        {/* on of the privet routs */}
+        />
         <Route
           path="/profile"
           element={<PrivateRoute Component={ProfileSection} />}
-        ></Route>
-        <Route path="/" element={<Landing />}></Route>
+        />
 
-        <Route path="/" element={<Landing />} />
+        {/* Catch-all Route */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -1,23 +1,13 @@
 import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
-// Define the schema for transaction history items
-const transactionHistoryItemSchema = new Schema({
-    amount: { type: Number, required: true, min: 0 },
-    date: { type: Date, required: true },
-    description: { type: String, required: true }
-}, { timestamps: true }); // Disable _id for subdocuments
-
-// Define the transaction schema
-const transactionSchema = new Schema({
-    title: { type: String, required: true },
-    category: { type: String, required: true },
-    dateOfCreation: { type: Date, required: true },
-    lastUpdate: { type: Date, required: true },
-    budget: { type: Number, required: true, min: 0 },
-    image: { type: String, default: null },
-    transactionsHistorique: [transactionHistoryItemSchema] // Array of transaction history items
+const transactionItemSchema = new Schema({
+    name: { type: String, required: true },
+    budget: { type: Number, required: true },
+    dateCreated: { type: Date, default: Date.now },
+    dateUpdated: { type: Date, default: Date.now },
+    amountAchieved: { type: Number, default: 0 },
+    profileId: { type: Schema.Types.ObjectId, ref: 'Profile', required: true },
 }, { timestamps: true });
 
-// Create the model from the schema
-export const Transaction = model('Transaction', transactionSchema);
+export const TransactionItem = model('TransactionItem', transactionItemSchema);
